67cd54f94e6b535e43228d84ccba438efaf0c21e
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-03-07 21:27:25 -0500

    rewrote attribute and text node parsing/rendering

--- src/zen-node.ts
+++ src/zen-node.ts
@@ -21,27 +21,56 @@ export class ZenNode {
                 for (let i = 0; i < currentNode.attributes.length; i++) {
                     const currentAttribute = currentNode.attributes[i];
                     if (currentAttribute.textContent.indexOf(valueMarker) > -1) {
-                        this.children.push({
+                        // text node can contain more than one marker
+                        // must save the whole string with markers and
+                        // build whole string on each value change
+                        const text = currentAttribute.textContent;
+                        const valuesCount = (text.match(new RegExp(valueMarker, 'gi')) || []).length;
+
+                        const attributeValue = {
                             type: 'attribute',
                             container: currentAttribute,
-                            index: valueIndex,
-                            currentValue: currentAttribute.textContent,
-                            oldValue: null
-                        });
-                        valueIndex++;
+                            template: currentAttribute.textContent,
+                            values: []
+                        };
+
+                        // for each value marker, save its index in template string
+                        for (let i = 0; i < valuesCount; i++) {
+                            attributeValue.values.push({
+                                index: valueIndex,
+                                currentValue: valueMarker,
+                                oldValue: null
+                            });
+                            this.children.push(attributeValue);
+                            valueIndex++;
+                        }
                     }
                 }
             } else {
                 // if it's not an element, must be in a text position
                 if (currentNode.textContent.indexOf(valueMarker) > -1) {
-                    this.children.push({
+                    // text node can contain more than one marker
+                    // see how many value markers are within text node
+                    const text = currentNode.textContent;
+                    const valuesCount = (text.match(new RegExp(valueMarker, 'gi')) || []).length;
+
+                    const textValue = {
                         type: 'text',
-                        index: valueIndex,
                         container: currentNode,
-                        currentValue: currentNode.textContent,
-                        oldValue: null
-                    });
-                    valueIndex++;
+                        template: currentNode.textContent,
+                        values: []
+                    };
+
+                    // for each value marker, save its index in template string
+                    for (let i = 0; i < valuesCount; i++) {
+                        textValue.values.push({
+                            index: valueIndex,
+                            currentValue: valueMarker,
+                            oldValue: null
+                        });
+                        this.children.push(textValue);
+                        valueIndex++;
+                    }
                 }
             }
         }
@@ -51,14 +80,34 @@ export class ZenNode {
         values.forEach((value, i) => {
             // grab node saved for value and update with new value
             const node = this.children[i];
-            node.oldValue = node.currentValue;
-            node.currentValue = value;
             switch (node.type) {
                 case 'attribute':
-                    (node.container as Attr).textContent = node.currentValue;
+                    const attribute = <Attr>node.container;
+                    // find the attribute part that corresponds with
+                    // this value index
+                    const attributeValues = node.values;
+                    const currentValue = attributeValues.find(val => val.index === i);
+                    currentValue.oldValue = currentValue.currentValue;
+                    currentValue.currentValue = value;
+                    // rebuild template from all current values
+                    let str = node.template;
+                    for (let j = 0; j < attributeValues.length; j++) {
+                        str = str.replace(valueMarker, attributeValues[j].currentValue);
+                    }
+                    attribute.textContent = str;
                     break;
                 case 'text':
-                    (node.container as Element).textContent = node.currentValue;
+                    // find the attribute part that corresponds with
+                    // this value index
+                    const nodeValue = node.values.find(asdf => asdf.index === i);
+                    nodeValue.oldValue = nodeValue.currentValue;
+                    nodeValue.currentValue = value;
+                    // rebuild template from all current values
+                    let newTextContent = node.template;
+                    for (let j = 0; j < node.values.length; j++) {
+                        newTextContent = newTextContent.replace(valueMarker, node.values[j].currentValue);
+                    }
+                    node.container.textContent = newTextContent;
                     break;
             }
         });
--- test/zen-render.test.js
+++ test/zen-render.test.js
@@ -13,9 +13,11 @@ describe('zen-render', function () {
     });
 
     it('should render zen template into dom with given values', function () {
-        const className = 'test-1';
-        const message = 'Hi again!';
-        const zenTemplate = zen`<div id="rendered-element-2" class="${ className }">${ message }</div>`;
+      const className1 = 'test-1';
+      const className2 = 'test-2';
+      const message = 'Test 1';
+        const number = 123;
+        const zenTemplate = zen`<div id="rendered-element-2" class="${ className1 } break ${ className2 }">${ message } - ${number}</div>`;
 
         // render into dom
         render(zenTemplate, document.querySelector('#render-container-2'));
@@ -24,13 +26,13 @@ describe('zen-render', function () {
         const renderedElement = document.querySelector('#rendered-element-2');
         const classes = renderedElement.className;
         const text = renderedElement.textContent;
-        chai.expect(classes).to.contain(className);
-        chai.expect(text).to.contain(message);
+        chai.expect(classes).to.contain(`${ className1 } break ${ className2 }`);
+        chai.expect(text).to.contain(`${ message } - ${number}`);
       });
 
     it('should render zen template into dom with updated values', function () {
       const className = 'test';
-      const message = 'Hi again!';
+      const message = 'Test 2';
       const zenTemplate = zen`<div id="rendered-element-3" class="${ className }">${ message }</div>`;
       render(zenTemplate, document.querySelector('#render-container-3'));
 
