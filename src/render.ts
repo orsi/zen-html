48e536d297ba0d7941c4236686d6c2550fffbd51
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-03-09 23:19:57 -0500

    add jsdoc comments

--- src/render.ts
+++ src/render.ts
@@ -2,6 +2,12 @@ import { ZenNode } from './zen-node';
 import { ZenTemplate } from './zen-template';
 
 export const containerCache = new WeakMap<Node, ZenNode>();
+
+/**
+ * Renders a zen template into a container DOM element.
+ * @param zenTemplate A zen template to render into the DOM
+ * @param container The DOM element to render into
+ */
 export const render = function (zenTemplate: ZenTemplate, container: Node) {
     // check if zen template has been rendered and cached
     let zenRender = containerCache.get(container);
