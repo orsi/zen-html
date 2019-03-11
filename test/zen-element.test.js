3b143db529adc8f665cde0266eaf344dbbaa999e
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-03-11 19:44:55 -0400

    update test descriptions

--- test/zen-element.test.js
+++ test/zen-element.test.js
@@ -32,23 +32,22 @@ describe('zen-element.ts', function () {
     zenTestElement.remove();
   });
 
-  it('should have properties object', function () {
+  it('should have properties property', function () {
     const zenTestElement = document.createElement('zen-test');
     chai.expect(zenTestElement.properties).to.exist;
   });
 
-  it('should have state object', function () {
+  it('should have state property', function () {
     const zenTestElement = document.createElement('zen-test');
     chai.expect(zenTestElement.state).to.exist;
   });
 
-  it('should have attribute property', function () {
+  it('should have test property', function () {
     const zenTestElement = document.createElement('zen-test');
-    zenTestElement.setAttribute('test', 'test');
     chai.expect(zenTestElement.properties.test).to.exist;
   });
 
-  it('should have an attribute property with given value', function () {
+  it('should have test property with given value', function () {
     const zenTestElement = document.createElement('zen-test');
     zenTestElement.setAttribute('test', 'test');
     chai.expect(zenTestElement.properties.test).to.contain('test');
