e8e5341e26aea1f43d2ae796cd8a0c6319001ae5
Jonathon Orsi <jonathon.orsi@gmail.com>
2022-10-31 22:30:25 -0400

    begin setup of react-like component syntax

--- src/zen.test.ts
+++ src/zen.test.ts
@@ -1,9 +1,26 @@
-import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
-import { zen } from "./zen.ts";
+import { assertExists } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+import { useEffect, useReference, useState, zen } from "./zen.ts";
+
+function MyComponent() {
+  const testState = useState(1);
+  const testReference = useReference(true);
+
+  useEffect(() => {
+    console.log("effect");
+    return () => {
+      console.log("cleanup");
+    };
+  });
+
+  return zen`<div>
+  <h1>zen-html</h1>
+  <div>useState: ${testState}</div>
+  <div>useReference: ${testReference.current}</div>
+</div>`;
+}
 
 Deno.test("#1", () => {
-  const x = zen`<h1>hi</h1>`;
-  const y = zen`<h1>hi</h1>`;
-  console.log(x, y);
-  assertEquals(x, y);
+  const dynamicComponent = MyComponent();
+  console.log(dynamicComponent);
+  assertExists(dynamicComponent);
 });
--- src/zen.ts
+++ src/zen.ts
@@ -1,19 +1,48 @@
-import { ZenTemplate } from "./zen-template.ts";
+export const DYNAMIC_MARKER = "❍";
 
 export const zenTemplateCache = new WeakMap<
   TemplateStringsArray,
-  ZenTemplate
+  string
 >();
+
+/**
+ * Parses the template strings and returns a string representation
+ * of an element with value positions replaced by markers.
+ * @param templateStrings
+ */
+function parse(templateStrings: TemplateStringsArray, values: unknown[]) {
+  let htmlString = "";
+  templateStrings.forEach((item, i) => {
+    htmlString += item + (i < templateStrings.length - 1 ? values[i] : "");
+  });
+  return htmlString;
+}
+
+export const useEffect = (effectFunction: () => void | (() => void)) => {
+  const effectCleanupFunction = effectFunction();
+  if (effectCleanupFunction) {
+    effectCleanupFunction();
+  }
+};
+export const useReference = (value: unknown) => {
+  return {
+    current: value,
+  };
+};
+export const useState = (value: unknown) => {
+  return value;
+};
+
 export const zen = function (
-  strings: TemplateStringsArray,
+  templateStrings: TemplateStringsArray,
   ...values: unknown[]
 ) {
-  // check if this template string array has been cached
-  let zenTemplate = zenTemplateCache.get(strings);
-  if (!zenTemplate) {
+  // check if this component been cached
+  let component = zenTemplateCache.get(templateStrings);
+  if (!component) {
     // create and cache template
-    zenTemplate = new ZenTemplate(strings, values);
-    zenTemplateCache.set(strings, zenTemplate);
+    component = parse(templateStrings, values);
+    zenTemplateCache.set(templateStrings, component);
   }
-  return zenTemplate;
+  return component;
 };
